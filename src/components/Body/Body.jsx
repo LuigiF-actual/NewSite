65c0d4e15f88866fa3c08664c24f909d2c7000b2
luigisp2 <luigifronada@gmail.com>
2026-03-07 10:35:16 +0100

    First commit

---
+++ src/components/Body/Body.jsx
@@ -0,0 +1,22 @@
+
+const Body = () => {
+
+    return (
+
+        <>
+            <BackGround />
+            daw
+        </>
+
+    )
+
+}
+
+export default Body;
+
+const BackGround = () => {
+  return (
+    <div className="bg-[url('image/BackGround.png')] bg-cover bg-center min-h-screen border-4">
+    </div>
+  );
+};
